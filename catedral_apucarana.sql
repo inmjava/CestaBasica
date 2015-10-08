f7afa49b33c03ba02b16e4d0d7d9214b6ea83ebe
inmjava <inmjava@yahoo.com.br>
2015-10-08 16:51:52 -0300

    Update catedral_apucarana.sql
    Adição dos usuários das paróquias

--- catedral_apucarana.sql
+++ catedral_apucarana.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
+-- version 3.4.11.1deb2.2
 -- http://www.phpmyadmin.net
 --
--- Servidor: 127.0.0.1
--- Tempo de Geração: 
--- Versão do Servidor: 5.5.27
--- Versão do PHP: 5.4.7
+-- Servidor: mysql01.cesta2.hospedagemdesites.ws
+-- Tempo de Geração: 08/10/2015 às 16:50:49
+-- Versão do Servidor: 5.6.21
+-- Versão do PHP: 5.3.3-7+squeeze27
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Banco de Dados: `catedral_apucarana`
+-- Banco de Dados: `cesta2`
 --
 
 -- --------------------------------------------------------
@@ -114,14 +114,25 @@ CREATE TABLE IF NOT EXISTS `tb_seg_usuario` (
   `nome` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `tb_seg_usuario_FKIndex1` (`cd_paroquia`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=88 ;
 
 --
 -- Extraindo dados da tabela `tb_seg_usuario`
 --
 
 INSERT INTO `tb_seg_usuario` (`id`, `cd_paroquia`, `login`, `senha`, `nome`) VALUES
-(1, 1, 'ivan', '0cc175b9c0f1b6a831c399e269772661', 'Ivan Nicoli');
+(76, 1, 'catnsl_apucarana', '6f2688a5fce7d48c8d19762b88c32c3b', 'Catedral Nossa Senhora De Lourdes'),
+(77, 2, 'coracaodemaria_apucarana', 'f5aa4bd09c07d8b2f65bad6c7cd3358f', 'Paróquia Imaculado Coração De Maria'),
+(78, 3, 'coracaoeucaristicodejesus', '65f2a94c8c2d56d5b43a1a3d9d811102', 'Paróquia Coração Eucarístico De Jesus'),
+(79, 4, 'cristoprofeta_apucarana', '2fa321697b50684a2f98ef21d0298991', 'Paróquia Cristo Profeta'),
+(80, 5, 'paroquiacristorei', 'e5b888ba51c1e2319b104a583a2906a2', 'Paróquia Cristo Rei'),
+(81, 6, 'cristosacerdoteapucarana', '60c97bef031ec312b512c08565c1868e', 'Paróquia Cristo Sacerdote'),
+(82, 7, 'nsaparecidaapucarana', '1f3db82eb3cb07faf60e1b1fc03768df', 'Paróquia Nossa Senhora Aparecida'),
+(83, 8, 'nossasenhoradefatima', '53f95fc35cd5c904da333d9231d67026', 'Paróquia Nossa Senhora De Fátima'),
+(84, 9, 'perpetuoapucarana', '996009f2374006606f4c0b0fda878af1', 'Paróquia Nossa Senhora Do Perpétuo Socorro'),
+(85, 10, 'saobenedito_apucarana', '670b5644a548cf40cd07963ac4c37e4e', 'Paróquia São Benedito'),
+(86, 11, 'parsfxavier_apucarana', '0a7c93e6c36f4d639a05adb387947066', 'Paróquia São Francisco Xavier'),
+(87, 12, 'santuario', '01588f3853f3a4683c9e134bd6f83fee', 'Santuário São José');
 
 -- --------------------------------------------------------
 
